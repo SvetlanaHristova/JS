4bd138ae8bd1a94fca00cae948b6542ee905841c
Svetlana Hristova <svetlanavhristova@gmail.com>
2022-07-30 15:59:15 +0300

    Add files via upload

---
+++ amazingNumbers2.js
@@ -0,0 +1,14 @@
+function amazingNumbers2(num) {
+    let numToString = num.toString();
+    let sum = 0;
+    for (i = 0; i < numToString.length; i++) {
+        sum += Number(numToString[i]);
+    }
+    let result = sum.toString().includes('9');
+    console.log(result 
+        ? `${num} Amazing? True`
+        : `${num} Amazing? False`) 
+       
+}
+amazingNumbers2(1233)
+amazingNumbers2(999)
